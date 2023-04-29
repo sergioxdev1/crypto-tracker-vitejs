04b2259a0e9453d3c4a7a94ddd9fc1df876ee47d
Sergio Carrasco <50931929+sergioxdev1@users.noreply.github.com>
2023-04-28 17:48:01 -0700

    Filter by coin name and some fixes

--- crypto-tracker-sumi/src/components/Coin.jsx
+++ crypto-tracker-sumi/src/components/Coin.jsx
@@ -1,15 +1,22 @@
+import { currencyFormat } from "../util";
 
 const Coin = ({ coin }) => {
-    console.log(coin);
 
     return (
         <>
-            <div className='grid grid-cols-3 md:grid-cols-4 font-light p-2 rounded border-gray-200 border-b hover:bg-yellow-300'>
-                <div className='flex items-center gap-1 w-full'>
-                    <img className='w-6' src={coin.image} alt={coin.name} />
+            <div className="grid grid-cols-3 sm:grid-cols-4 font-light p-2 m-2 rounded border-gray-200 border-2 hover:bg-gray-200">
+                <div className="flex items-center gap-1 w-full">
+                    <img className="w-6" src={coin.image} alt={coin.name} />
                     <p>{coin.name}</p>
+                    <span className="text-xs">({coin.symbol})</span>
                 </div>
-                <span>{coin.current_price}</span>
+                <span className="w-full text-left">{currencyFormat(coin.current_price)}</span>
+                <span className={`flex gap-1 ${coin.price_change_percentage_24h < 0 ? 'text-red-400' : 'text-green-400'}`}>
+                    {coin.price_change_percentage_24h}
+                </span>
+                <div className="hidden sm:block">
+                    <span>{currencyFormat(coin.market_cap)}</span>
+                </div>
             </div>
         </>
     )
--- crypto-tracker-sumi/src/components/TableMarketCoin.jsx
+++ crypto-tracker-sumi/src/components/TableMarketCoin.jsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
 import useAxios from '../hooks/useAxios'
 import Coin from './Coin';
 
 const TableMarketCoin = () => {
     const { response } = useAxios('coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false');
-    //console.log(response);
+    const [search, setSearch] = useState('');
+    console.log(search)
+    let filteredCoin;
+    if (response) {
+        filteredCoin = response.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
+    }
 
     return (
         <section className='mt-8'>
-            <h1 className='text-xl text-bold mb-2'> Markets </h1>
-            {response && response.map(coin => <Coin key={coin.id} coin={coin} />)}
+            <h1 className='text-xl text-bold mb-2'> Markets: </h1>
+            <div className='flex justify-end '>
+                <label className='font-light flex items-center gap-1'>Search:</label>
+                <input type="text" placeholder='coin name...' className="font-light p-2 m-2 rounded bg-gray-100 border-gray-300 border-2"
+                    onChange={e => setSearch(e.target.value)} />
+            </div>
+            <div className="grid grid-cols-3 sm:grid-cols-4 font-light p-2 rounded border-gray-200 w-full">
+                <div className="flex items-center gap-1 w-8">
+                    Coin:
+                </div>
+                <div className="flex items-center gap-1 w-full">
+                    Price (USD):
+                </div>
+                <div className="flex items-center gap-1 w-full">
+                    Trending (Last 24 hrs):
+                </div>
+                <div className="flex items-center gap-1 w-full">
+                    Market Cap (USD):
+                </div>
+            </div>
+            {filteredCoin && filteredCoin.map(coin => <Coin key={coin.id} coin={coin} />)}
         </section>
     )
 }
